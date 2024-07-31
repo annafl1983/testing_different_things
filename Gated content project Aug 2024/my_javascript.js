5cbff36a6bec89989d002b28fe0a6001f7397867
annafl1983 <114492637+annafl1983@users.noreply.github.com>
2024-07-31 15:28:49 +0000

    yes

--- Gated content project Aug 2024/my_javascript.js
+++ Gated content project Aug 2024/my_javascript.js
@@ -1,4 +1,6 @@
-var iframe1 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/DHfRfU3XUEo?si=NhnIKbVaWV7Itibq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+var content1 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/DHfRfU3XUEo?si=NhnIKbVaWV7Itibq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 
 
-var iframe2 = `<iframe>Does this work with just text or not? Let's see</iframe>`
+var content2 = `<iframe>Does this work with just text or not? Let's see</iframe>`;
+
+var content3 = `<p>This is a paragrapah</p>`;
