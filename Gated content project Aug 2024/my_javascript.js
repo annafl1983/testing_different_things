a56ec579ff2e309ce7f7e168b8175367c8b821cc
annafl1983 <114492637+annafl1983@users.noreply.github.com>
2024-07-31 15:23:40 +0000

    yes please

--- Gated content project Aug 2024/my_javascript.js
+++ Gated content project Aug 2024/my_javascript.js
@@ -1,10 +1,4 @@
+var iframe1 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/DHfRfU3XUEo?si=NhnIKbVaWV7Itibq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
 
-function openArticle() {
-    console.log("yay! You submitted the form! Good cat!");
-}
 
-document.addEventListener("submit", function(event)
-{
-    event.preventDefault();
-    openArticle()
-}, false); 
+var iframe2 = `<iframe>Does this work with just text or not? Let's see</iframe>`
